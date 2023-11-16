3aa87ff0be2ba219b957265cd7188f328a9ab1b3
JuanRoccia <juanroccia@gmail.com>
2023-11-16 00:50:11 -0300

    url.routes.js

--- src/controllers/url.routes.js
+++ src/controllers/url.routes.js
@@ -1,4 +1,4 @@
-const url = "https://100.20.92.101:1000/user/";
+const url = "https://100.20.92.101:5000/user/";
 const user_id = localStorage.getItem("id");
 const getBillsUrl = url + `${user_id}/sales`;
 const getClientsUrl = url + `${user_id}/clients`;
@@ -10,7 +10,7 @@ const getServicesUrl = url + `${user_id}/services`;
 // const oneServiceUrl = `http://127.0.0.1:5000/user/${user_id}/products/${serviceId}`
 // const oneBillUrl = `http://127.0.0.1:5000/user/${user_id}/sales/${billId}`;
 
-const loginUrl = `https://100.20.92.101:1000/user/login`
+const loginUrl = `https://100.20.92.101:5000/user/login`
 
 export default { getBillsUrl, getClientsUrl, getProductsUrl, getServicesUrl, loginUrl };
 
