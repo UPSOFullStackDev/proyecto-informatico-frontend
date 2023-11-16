5513a0564d937cd7d3a18c9ee3f4c0b8e7c667b5
JuanRoccia <juanroccia@gmail.com>
2023-11-15 22:19:35 -0300

    api url

--- src/controllers/url.routes.js
+++ src/controllers/url.routes.js
@@ -1,14 +1,15 @@
+const url = "https://proyecto-informatico-backend.vercel.app/user/";
 const user_id = localStorage.getItem("id");
-const getBillsUrl = `http://127.0.0.1:5000/user/${user_id}/sales`;
-const getClientsUrl = `http://127.0.0.1:5000/user/${user_id}/clients`;
-const getProductsUrl = `http://127.0.0.1:5000/user/${user_id}/products`;
-const getServicesUrl = `http://127.0.0.1:5000/user/${user_id}/services`;
+const getBillsUrl = url + `${user_id}/sales`;
+const getClientsUrl = url + `${user_id}/clients`;
+const getProductsUrl = url + `${user_id}/products`;
+const getServicesUrl = url + `${user_id}/services`;
 
 // const oneClientUrl = `http://127.0.0.1:5000/user/${user_id}/clients/${clientId}`
 // const oneProductUrl = `http://127.0.0.1:5000/user/${user_id}/products/${productId}`
 // const oneServiceUrl = `http://127.0.0.1:5000/user/${user_id}/products/${serviceId}`
 // const oneBillUrl = `http://127.0.0.1:5000/user/${user_id}/sales/${billId}`;
 
-const loginUrl = `http://127.0.0.1:5000/user/login`
+const loginUrl = url + `login`
 
 export default { getBillsUrl, getClientsUrl, getProductsUrl, getServicesUrl, loginUrl };
